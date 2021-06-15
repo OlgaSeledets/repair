126bcc1ed1c9e56a72d1addae8e3ef69b6d0e018
Olgs <seledets.olya@gmail.com>
2021-06-15 12:35:20 +0300

    Add adaptive in the section photos, made a slider. Add changes to other sections.

---
+++ src/js/slider.js
@@ -0,0 +1,22 @@
+$('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.arrows__left'),
+    nextArrow: $('.arrows__right'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
